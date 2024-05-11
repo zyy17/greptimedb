b62d3ece62dce7b93fce806885d00912c8d43bd2
zyy17 <zyylsxm@gmail.com>
2024-05-11 16:36:37 +0800

    fix: clippy errors

--- src/cmd/src/standalone.rs
+++ src/cmd/src/standalone.rs
@@ -290,6 +290,7 @@ impl StartCommand {
                     self.config_file.as_deref(),
                     self.env_prefix.as_ref(),
                 )
+                .map_err(Box::new)
                 .context(LoadLayeredConfigSnafu)?,
             )?,
         )))
