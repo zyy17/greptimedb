3250254c2486f00f301e0302e7ee59b0fd5302d2
zyy17 <zyylsxm@gmail.com>
2025-06-19 15:28:43 +0800

    feat: create built-in flows
    Signed-off-by: zyy17 <zyylsxm@gmail.com>

--- src/frontend/src/instance/trace_handler.rs
+++ src/frontend/src/instance/trace_handler.rs
@@ -19,24 +19,36 @@ use api::v1::{
     TableName,
 };
 use client::Output;
-use common_catalog::consts::{default_engine, TRACE_TABLE_NAME};
+use common_catalog::consts::{
+    default_engine, PARENT_SPAN_ID_COLUMN, SERVICE_NAME_COLUMN, SPAN_ID_COLUMN, TRACE_TABLE_NAME,
+};
 use common_error::ext::BoxedError;
-use common_telemetry::info;
+use common_telemetry::{debug, info, warn};
 use servers::error::{
     CatalogSnafu, ExecuteGrpcRequestSnafu, InFlightWriteBytesExceededSnafu, Result as ServerResult,
     TableOperationSnafu,
 };
+use servers::otlp::trace::TIMESTAMP_COLUMN;
 use session::context::QueryContextRef;
 use snafu::ResultExt;
-use store_api::mito_engine_options::APPEND_MODE_KEY;
 use table::TableRef;
 use tokio::sync::OnceCell;
 
 use crate::instance::Instance;
 
-const OTEL_TRACES_SERVICES_RED_METRICS_1M_TABLE_NAME: &str = "otel_traces_services_red_metrics_1m";
+const OPENTELEMETRY_TRACES_DEPENDENCIES_1H_TABLE_NAME: &str =
+    "opentelemetry_traces_dependencies_1h";
 
-static START: OnceCell<()> = OnceCell::const_new();
+const PARENT_SERVICE_COLUMN: &str = "parent_service";
+const CHILD_SERVICE_COLUMN: &str = "child_service";
+const CALL_COUNT_COLUMN: &str = "call_count";
+const TIME_WINDOW_COLUMN: &str = "time_window";
+const UPDATED_AT_COLUMN: &str = "updated_at";
+
+const OPENTELEMETRY_TRACES_DEPENDENCIES_1H_FLOW_NAME: &str =
+    "opentelemetry_traces_dependencies_1h_flow";
+
+static CREATE_BUILT_IN_FLOWS: OnceCell<()> = OnceCell::const_new();
 
 impl Instance {
     pub async fn handle_trace_inserts(
@@ -61,44 +73,50 @@ impl Instance {
             .map_err(BoxedError::new)
             .context(ExecuteGrpcRequestSnafu);
 
-        START
+        CREATE_BUILT_IN_FLOWS
             .get_or_init(|| async {
-                self.create_builtin_traces_flows(ctx.clone()).await.unwrap();
+                self.create_sink_tables(ctx.clone()).await;
+                self.create_flows(ctx.clone()).await;
             })
             .await;
-
-        self.create_flows(ctx).await.unwrap();
 
         result
     }
 
-    async fn create_builtin_traces_flows(&self, ctx: QueryContextRef) -> ServerResult<TableRef> {
-        if let Some(table) = self
-            .catalog_manager
-            .table(
-                ctx.current_catalog(),
-                "public",
-                OTEL_TRACES_SERVICES_RED_METRICS_1M_TABLE_NAME,
-                Some(&ctx),
-            )
-            .await
-            .context(CatalogSnafu)?
+    async fn create_sink_tables(&self, query_ctx: QueryContextRef) {
+        for table_expr in
+            self.create_table_exprs(query_ctx.current_catalog(), &query_ctx.current_schema())
         {
-            return Ok(table);
-        }
+            let catalog_name = &query_ctx.current_catalog();
+            let schema_name = &query_ctx.current_schema();
+            let table_name = table_expr.table_name.clone();
 
-        // Create the system table if it doesn't exist.
-        self.create_sink_tables(ctx.clone()).await
+            debug!(
+                "Creating the table '{}' in schema '{}' catalog '{}'",
+                table_name, schema_name, catalog_name
+            );
+
+            if let Err(e) = self.do_create_table(query_ctx.clone(), table_expr).await {
+                warn!(e; "Failed to create table '{}' in schema '{}' catalog '{}'",
+                    table_name, schema_name, catalog_name
+                );
+            }
+        }
     }
 
-    async fn create_sink_tables(&self, query_ctx: QueryContextRef) -> ServerResult<TableRef> {
-        let mut create_table_expr = self.build_create_table_expr(query_ctx.current_catalog());
+    async fn do_create_table(
+        &self,
+        query_ctx: QueryContextRef,
+        mut table_expr: CreateTableExpr,
+    ) -> ServerResult<TableRef> {
+        debug!("Creating the table for built-in flows '{:?}'", table_expr);
+
         if let Some(table) = self
             .catalog_manager
             .table(
-                &create_table_expr.catalog_name,
-                &create_table_expr.schema_name,
-                &create_table_expr.table_name,
+                &table_expr.catalog_name,
+                &table_expr.schema_name,
+                &table_expr.table_name,
                 Some(&query_ctx),
             )
             .await
@@ -108,75 +126,63 @@ impl Instance {
             return Ok(table);
         }
 
-        // Create the `slow_queries` system table.
+        // Create the table.
         let table = self
             .statement_executor
-            .create_table_inner(&mut create_table_expr, None, query_ctx.clone())
+            .create_table_inner(&mut table_expr, None, query_ctx.clone())
             .await
             .context(TableOperationSnafu)?;
 
         info!(
-            "Create the {} table in {:?} successfully.",
-            OTEL_TRACES_SERVICES_RED_METRICS_1M_TABLE_NAME, "public"
+            "Create the '{}' table in schema '{}' catalog '{}' successfully.",
+            table_expr.table_name, table_expr.schema_name, table_expr.catalog_name
         );
 
         Ok(table)
     }
 
-    fn build_create_table_expr(&self, catalog: &str) -> CreateTableExpr {
+    fn create_table_exprs(&self, catalog: &str, schema: &str) -> Vec<CreateTableExpr> {
+        vec![self.dependencies_1h_table_create_table_expr(catalog, schema)]
+    }
+
+    fn dependencies_1h_table_create_table_expr(
+        &self,
+        catalog: &str,
+        schema: &str,
+    ) -> CreateTableExpr {
         let column_defs = vec![
             ColumnDef {
-                name: "service_name".to_string(),
+                name: PARENT_SERVICE_COLUMN.to_string(),
                 data_type: ColumnDataType::String as i32,
                 is_nullable: false,
                 default_constraint: vec![],
                 semantic_type: SemanticType::Field as i32,
-                comment: "The name of the service".to_string(),
+                comment: "The name of the parent service".to_string(),
                 datatype_extension: None,
                 options: None,
             },
             ColumnDef {
-                name: "total_count".to_string(),
+                name: CHILD_SERVICE_COLUMN.to_string(),
+                data_type: ColumnDataType::String as i32,
+                is_nullable: false,
+                default_constraint: vec![],
+                semantic_type: SemanticType::Field as i32,
+                comment: "The name of the child service".to_string(),
+                datatype_extension: None,
+                options: None,
+            },
+            ColumnDef {
+                name: CALL_COUNT_COLUMN.to_string(),
                 data_type: ColumnDataType::Int64 as i32,
                 is_nullable: false,
                 default_constraint: vec![],
                 semantic_type: SemanticType::Field as i32,
-                comment: "The total count of traces".to_string(),
+                comment: "The number of calls between parent and child services".to_string(),
                 datatype_extension: None,
                 options: None,
             },
             ColumnDef {
-                name: "error_count".to_string(),
-                data_type: ColumnDataType::Int64 as i32,
-                is_nullable: false,
-                default_constraint: vec![],
-                semantic_type: SemanticType::Field as i32,
-                comment: "The error count of traces".to_string(),
-                datatype_extension: None,
-                options: None,
-            },
-            ColumnDef {
-                name: "avg_latency_nano".to_string(),
-                data_type: ColumnDataType::Float64 as i32,
-                is_nullable: false,
-                default_constraint: vec![],
-                semantic_type: SemanticType::Field as i32,
-                comment: "The average latency of traces in nanoseconds".to_string(),
-                datatype_extension: None,
-                options: None,
-            },
-            ColumnDef {
-                name: "latency_sketch".to_string(),
-                data_type: ColumnDataType::Binary as i32,
-                is_nullable: false,
-                default_constraint: vec![],
-                semantic_type: SemanticType::Field as i32,
-                comment: "The latency sketch of traces".to_string(),
-                datatype_extension: None,
-                options: None,
-            },
-            ColumnDef {
-                name: "time_window".to_string(),
+                name: TIME_WINDOW_COLUMN.to_string(),
                 data_type: ColumnDataType::TimestampNanosecond as i32,
                 is_nullable: false,
                 default_constraint: vec![],
@@ -186,7 +192,7 @@ impl Instance {
                 options: None,
             },
             ColumnDef {
-                name: "updated_at".to_string(),
+                name: UPDATED_AT_COLUMN.to_string(),
                 data_type: ColumnDataType::TimestampNanosecond as i32,
                 is_nullable: false,
                 default_constraint: vec![],
@@ -197,64 +203,100 @@ impl Instance {
             },
         ];
 
-        let table_options = HashMap::from([(APPEND_MODE_KEY.to_string(), "true".to_string())]);
-
         CreateTableExpr {
             catalog_name: catalog.to_string(),
-            schema_name: "public".to_string(),
-            table_name: OTEL_TRACES_SERVICES_RED_METRICS_1M_TABLE_NAME.to_string(),
+            schema_name: schema.to_string(),
+            table_name: OPENTELEMETRY_TRACES_DEPENDENCIES_1H_TABLE_NAME.to_string(),
             column_defs,
-            time_index: "time_window".to_string(),
+            time_index: TIME_WINDOW_COLUMN.to_string(),
             primary_keys: vec![],
             create_if_not_exists: true,
-            table_options,
+            table_options: HashMap::new(),
             table_id: None,
-            desc: "GreptimeDB system table for storing traces".to_string(),
+            desc: "The table for storing traces dependencies for 1 hour aggregation".to_string(),
             engine: default_engine().to_string(),
         }
     }
 
-    async fn create_flows(&self, query_ctx: QueryContextRef) -> ServerResult<Output> {
-        let create_flow_expr = self.build_create_flow_expr(query_ctx.current_catalog());
+    async fn create_flows(&self, query_ctx: QueryContextRef) {
+        for flow_expr in
+            self.create_flow_exprs(query_ctx.current_catalog(), &query_ctx.current_schema())
+        {
+            let catalog_name = &query_ctx.current_catalog();
+            let schema_name = &query_ctx.current_schema();
+            let flow_name = flow_expr.flow_name.clone();
 
-        // Create the `slow_queries` system table.
-        let table = self
-            .statement_executor
-            .create_flow_inner(create_flow_expr, query_ctx.clone())
-            .await
-            .context(TableOperationSnafu)?;
+            debug!(
+                "Creating the flow '{}' in schema '{}' catalog '{}'",
+                flow_name, schema_name, catalog_name
+            );
 
-        Ok(table)
+            if let Err(e) = self.do_create_flow(query_ctx.clone(), flow_expr).await {
+                warn!(e; "Failed to create the flow '{}' in schema '{}' catalog '{}'",
+                    flow_name, schema_name, catalog_name
+                );
+            }
+        }
     }
 
-    fn build_create_flow_expr(&self, catalog: &str) -> CreateFlowExpr {
-        let sql = r#"SELECT
-        service_name,
-        count(*) as total_count,
-        sum(case when span_status_code = 'STATUS_CODE_ERROR' then 1 else 0 end) as error_count,
-        avg(duration_nano) as avg_latency_nano,
-        uddsketch_state(128, 0.01, duration_nano) AS latency_sketch,
-        date_bin('1 minutes'::INTERVAL, timestamp, '2025-05-17 00:00:00') as time_window
-    FROM opentelemetry_traces GROUP BY service_name, time_window"#;
+    async fn do_create_flow(
+        &self,
+        query_ctx: QueryContextRef,
+        create_flow_expr: CreateFlowExpr,
+    ) -> ServerResult<Output> {
+        debug!("Creating the flow '{:?}'", create_flow_expr);
+
+        // FIXME(zyy17): How to make sure the flow is initialized?
+        self.statement_executor
+            .create_flow_inner(create_flow_expr, query_ctx.clone())
+            .await
+            .context(TableOperationSnafu)
+    }
+
+    fn create_flow_exprs(&self, catalog: &str, schema: &str) -> Vec<CreateFlowExpr> {
+        vec![self.dependencies_1h_create_flow_expr(catalog, schema)]
+    }
+
+    fn dependencies_1h_create_flow_expr(&self, catalog: &str, schema: &str) -> CreateFlowExpr {
+        let sql = format!(
+            r#"SELECT
+                parent.{} as parent_service,
+                child.{} as child_service,
+                count(*) as {},
+                date_bin('1 hours'::INTERVAL, parent.{}, '2025-05-17 00:00:00') as {}
+            FROM {} as parent
+            JOIN {} as child ON child.{} = parent.{}
+            GROUP BY parent_service, child_service, {}"#,
+            SERVICE_NAME_COLUMN,
+            SERVICE_NAME_COLUMN,
+            CALL_COUNT_COLUMN,
+            TIMESTAMP_COLUMN,
+            TIME_WINDOW_COLUMN,
+            TRACE_TABLE_NAME,
+            TRACE_TABLE_NAME,
+            PARENT_SPAN_ID_COLUMN,
+            SPAN_ID_COLUMN,
+            TIME_WINDOW_COLUMN
+        );
 
         CreateFlowExpr {
             catalog_name: catalog.to_string(),
-            flow_name: "test_flow".to_string(),
+            flow_name: OPENTELEMETRY_TRACES_DEPENDENCIES_1H_FLOW_NAME.to_string(),
             source_table_names: vec![TableName {
                 catalog_name: catalog.to_string(),
-                schema_name: "public".to_string(),
+                schema_name: schema.to_string(),
                 table_name: TRACE_TABLE_NAME.to_string(),
             }],
             flow_options: HashMap::new(),
             sink_table_name: Some(TableName {
                 catalog_name: catalog.to_string(),
-                schema_name: "public".to_string(),
-                table_name: OTEL_TRACES_SERVICES_RED_METRICS_1M_TABLE_NAME.to_string(),
+                schema_name: schema.to_string(),
+                table_name: OPENTELEMETRY_TRACES_DEPENDENCIES_1H_TABLE_NAME.to_string(),
             }),
             or_replace: false,
             create_if_not_exists: true,
             expire_after: None,
-            comment: "".to_string(),
+            comment: "The flow for aggregating traces dependencies in 1 hour window".to_string(),
             sql: sql.to_string(),
         }
     }
