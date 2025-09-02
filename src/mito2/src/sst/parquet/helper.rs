ad020625857f87e64549d57ea1a0b1c71b0a77d7
zyy17 <zyylsxm@gmail.com>
2025-09-02 15:40:33 -0700

    chore: update configs

--- src/mito2/src/sst/parquet/helper.rs
+++ src/mito2/src/sst/parquet/helper.rs
@@ -90,7 +90,7 @@ fn parse_column_orders(
     }
 }
 
-const FETCH_PARALLELISM: usize = 8;
+const FETCH_PARALLELISM: usize = 16;
 pub(crate) const MERGE_GAP: usize = 512 * 1024;
 
 /// Asynchronously fetches byte ranges from an object store.
